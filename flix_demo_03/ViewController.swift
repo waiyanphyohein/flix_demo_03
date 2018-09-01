c31fc43eb1caa0578edb373b94633b8d41eb58b1
waiyanphyohein <waiyanphyohein@gmail.com>
2018-09-01 01:42:51 -0400

    Initial Commit

---
+++ flix_demo_03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix_demo_03
+//
+//  Created by WaiYanPhyo Hein on 9/1/18.
+//  Copyright © 2018 WaiYanPhyo Hein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
