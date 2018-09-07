895a4692d1a21bbc5b23a59d7405942b25434580
waiyanphyohein <waiyanphyohein@gmail.com>
2018-09-06 20:13:30 -0400

    Modified and Added Table View

--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
+use_frameworks!    # Instruct CocoaPods to use dynamic frameworks instead of static libraries for Pods
 
 target 'MyApp' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -12,4 +13,5 @@ target 'MyApp' do
   pod 'OAuthSwift', '~> 1.1.0'
   pod 'OAuthSwiftAlamofire'
   
+	
 end
---
+++ flix_demo_03/NowPlayingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NowPlayingViewController.swift
+//  flix_demo_03
+//
+//  Created by WaiYanPhyo Hein on 9/6/18.
+//  Copyright © 2018 WaiYanPhyo Hein. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10);
+        let session = URLSession(configuration: .default, delegate: nil , delegateQueue: OperationQueue.main);
+        let task = session.dataTask(with: request) { ( data, response, error) in
+            
+            if let error = error{
+                print(error.localizedDescription)
+            }else if let data = data{
+                let dataDictionary =  try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any];
+                let movies = dataDictionary["results"] as! [[String:  Any]];
+                for movie in movies {
+                    let title = movie["title"] as! String;
+                    print(title);
+                    
+                }
+            }
+        }
+        task.resume();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
