6937020482f088986b965e36a3711c52bb608a1d
waiyanphyohein <waiyanphyohein@gmail.com>
2018-09-10 19:31:12 -0400

    Added Table View with Updates to it

---
+++ flix_demo_03/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flix_demo_03
+//
+//  Created by WaiYanPhyo Hein on 9/10/18.
+//  Copyright © 2018 WaiYanPhyo Hein. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- flix_demo_03/NowPlayingViewController.swift
+++ flix_demo_03/NowPlayingViewController.swift
@@ -13,10 +13,12 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var movies:[[String:Any]] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         
-        tableView.dataSource = self as! UITableViewDataSource;
+        tableView.dataSource = self as? UITableViewDataSource;
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10);
         let session = URLSession(configuration: .default, delegate: nil , delegateQueue: OperationQueue.main);
@@ -25,24 +27,30 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
             if let error = error{
                 print(error.localizedDescription)
             }else if let data = data{
+                
                 let dataDictionary =  try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any];
                 let movies = dataDictionary["results"] as! [[String:  Any]];
-                for movie in movies {
-                    let title = movie["title"] as! String;
-                    print(title);
-                    
-                }
+                self.movies = movies;
+                self.tableView.reloadData();
             }
+            
         }
         task.resume();
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return movies.count ;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath);
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell;
         
+        let movie = movies[indexPath.row];
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        
+        cell.titleLabel.text = title;
+        cell.overviewLabel.text = overview;
         return cell;
     }
 
